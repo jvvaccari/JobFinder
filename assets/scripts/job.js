893f3828ef2d9c8ab0b25630ae561e7974092358
Cadu Souza <cadusouza14@gmail.com>
2024-07-31 14:58:09 -0300

    Initial job representation

---
+++ assets/scripts/job.js
@@ -0,0 +1,11 @@
+class Job {
+  constructor(title, description, enterprise, email, salary, date_created, id) {
+    this.title = title;
+    this.description = description;
+    this.enterprise = enterprise;
+    this.email = email;
+    this.salary = salary;
+    this.date_created = date_created;
+    this.id = id;
+  }
+}
