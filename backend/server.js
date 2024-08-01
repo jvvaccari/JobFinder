637a634abd069f782b521ff798cc8945d37ddfca
joao <joao.vaccari@majovdev.com.br>
2024-08-01 18:13:32 -0300

    configuring scripts js

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,4 @@ app.get('/', (req, res) => {
     res.sendFile(filePath);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
