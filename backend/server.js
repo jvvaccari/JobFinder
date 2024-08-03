70563bfb0f983ae995f87c8eed0464f1f6837967
Cadu Souza <cadusouza14@gmail.com>
2024-08-03 14:17:03 -0300

    Weird thingys prob resolved

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import { dirname,join } from "path";
-import { fileURLToPath } from 'url';
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 8080;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(join(__dirname, '../frontend')));
+app.use(express.static(join(__dirname, "../")));
 
-app.get('/', (req, res) => {
-    const filePath = join(__dirname,"../frontend/index.html");
+app.get("/", (req, res) => {
+    const filePath = join(__dirname, "../frontend/index.html");
     res.sendFile(filePath);
 });
 
