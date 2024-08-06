4a01934aedb389f0c734dd5f91321a047b2a8751
João Vaccari <jvvaccari1@gmail.com>
2024-08-06 08:45:49 -0300

    setting the col-sm size

--- frontend/assets/scripts/infoJobs.js
+++ frontend/assets/scripts/infoJobs.js
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 const jobElement = document.createElement("div");
                 
                 jobElement.innerHTML = `
-                    <div class="card d-flex flex-row justify-content-center align-items-center">
+                    <div class="card d-flex flex-row justify-content-center align-items-center p-5">
                         <img class="img-jobs" alt="company image" src="${job.image}"/>
-                        <div class="d-flex flex-column col-lg-9 col-md-6 px-2 mt-3">
+                        <div class="d-flex flex-column col-lg-9 col-md-6 col-sm-7 mt-3">
                             <p class="h6">${job.title}</p>
                             <p class="h2">${job.company}</p>
                             <p class="h7">R$${job.salary}</p>
